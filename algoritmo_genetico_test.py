99025dabedf584291ac91c84a7ab2993a3425bd3
lombax <anner.calvo@gmail.com>
2021-01-19 16:06:11 -0600

    cambios del merge v1.1

--- algoritmo_genetico_test.py
+++ algoritmo_genetico_test.py
@@ -6,6 +6,6 @@ from dominio_ag_tsp import DominioAGTSP
 class PruebaAlgoritmoGenetico(unittest.TestCase):
 
     def test_optimizar(self):
-        dominio = DominioAGTSP('datos/ciudades_cr_pruebas.csv', 'Alajuela')
+        dominio = DominioAGTSP('datos/ciudades_cr.csv', 'Alajuela')
         sol = optimizar(dominio, 100, 0.1, 0.5, 1000,False)
         self.assertTrue(dominio.validar(sol))
