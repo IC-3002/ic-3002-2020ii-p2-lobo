528ddea65d7283b31338746cf65d36e503fbcf3d
Jimmy Salas <ate.salas20@gmail.com>
2021-01-26 16:50:40 -0600

    modificaciones al cuaderno

--- prueba.py
+++ prueba.py
@@ -1,14 +1,14 @@
 import re
-from simulated_annealing import optimizar
-from dominio_tsp import DominioTSP
-#from dominio_ag_tsp import DominioAGTSP
-#from algoritmo_genetico import optimizar
+#from simulated_annealing import optimizar
+#from dominio_tsp import DominioTSP
+from dominio_ag_tsp import DominioAGTSP
+from algoritmo_genetico import optimizar
 from math import e
+from time import time
 
 
 
-# print(e)
-
+"""
 temperatura = 10000
 enfriamiento = [0.8,0.9,0.95,0.99]
 
@@ -20,9 +20,8 @@ while(temperatura < 10000000):
         print("|  17   |",  temperatura, "|", tasa,  "|",  round(solucion,3), "|", cont, "|")
 
     temperatura = temperatura/0.2        
+"""
 
-
-    
     
 """    
 poblacion = 100
@@ -46,3 +45,33 @@ while(poblacion <= 1000):
 
     poblacion = poblacion + 200        
 """
+
+
+#Annealing: 1250000.0	0.99	1762.4	1856    
+#Genetico: 700	0.1	1675.0	1000
+    
+
+
+#Simualted Annealing
+
+
+"""
+dominio = DominioTSP('datos/ciudades_cr.csv', 'Alajuela')
+for i in range(0,5):
+    start_time = time()
+    solucion = optimizar(dominio,1250000.0,0.99)
+    elapsed_time = time() - start_time
+    print("Elapsed time: %0.10f seconds." % elapsed_time)
+"""
+
+
+#Genetico
+
+dominio = DominioAGTSP('datos/ciudades_cr.csv', 'Alajuela')
+for i in range(0,5):
+    start_time = time()
+    solucion = optimizar(dominio,700,0.1,0.1,1000,False)
+    elapsed_time = time() - start_time
+    print("Elapsed time: %0.10f seconds." % elapsed_time)
+
+
